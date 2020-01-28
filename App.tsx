46f7b2a6f6c4834289acdea13614470078c18234
Praya Amadiga Pitasa <prayaamadigapitasa@gmail.com>
2020-01-28 14:04:59 +0700

    Update

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
-import {InputOTP} from './src';
+import {ButtonGroup} from 'react-native-design-kit';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View />
-      <InputOTP />
+      <ButtonGroup buttonIds={['Alpha', 'Beta']} onSelect={() => {}} />
     </View>
   );
 }
