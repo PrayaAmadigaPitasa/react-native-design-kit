6d053d03bcd1e684d1634a3703a0f4b6e052a662
Praya Amadiga Pitasa <prayaamadigapitasa@gmail.com>
2020-01-28 10:22:02 +0700

    Update Node

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
   root: true,
-  extends: ['@react-native-community'],
+  extends: [
+    '@react-native-community',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
   rules: {
     'react-hooks/exhaustive-deps': 'off',
   },
