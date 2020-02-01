298779c6657404b20b9d05287e998e5cc67c5a55
Praya Amadiga Pitasa <prayaamadigapitasa@gmail.com>
2020-02-01 13:01:59 +0700

    Update Input styles

--- App.tsx
+++ App.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
-import {Chip} from './src';
+import {Input} from './src';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Chip
-        horizontal
-        actionType="radio"
-        chips={['Place', 'Hotel', 'Road', 'Mansion', 'Highway', 'Store']}
-        horizontalScrollButton
-        onSelect={() => {}}
-      />
+      <Input rightIconAction="delete" label="Title" labelPosition="border" />
     </View>
   );
 }
