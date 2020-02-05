bfe535c7919ecdfec48824de1ec31f15cbb89b8d
Praya Amadiga Pitasa <prayaamadigapitasa@gmail.com>
2020-02-06 03:08:20 +0700

    Update Node

--- App.tsx
+++ App.tsx
@@ -1,37 +1,3 @@
 import StoryBook from './storybook';
 
 export default StoryBook;
-
-// import React from 'react';
-// import {View, StyleSheet, Text} from 'react-native';
-// import Icon from 'react-native-vector-icons/FontAwesome';
-// import {Picker, Input, Radio, Slider, Stepper} from './src';
-
-// Icon.loadFont();
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Picker
-//         data={['Banana', 'Melon', 'Orange']}
-//         keyExtractor={item => item}
-//         renderItem={({item}) => <Text>{item}</Text>}
-//         titleExtractor={item => item}
-//         onSelect={() => {}}
-//       />
-//       <Input label="Title" labelPosition="border" rightIconAction="delete" />
-//       <Radio radioIds={['Banana', 'Melon', 'Orange']} onSelect={() => {}} />
-//       <Slider button indicator onChangeValue={() => {}} />
-//       <Stepper />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     paddingHorizontal: 12,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
