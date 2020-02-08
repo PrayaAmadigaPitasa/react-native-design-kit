f7174d34e2ad03006a547d9feff11bf7249ea460
Praya Amadiga Pitasa <prayaamadigapitasa@gmail.com>
2020-02-08 10:41:44 +0700

    Update ChipAdvancedStory

--- storybook/stories/chip/ChipAdvancedStory.tsx
+++ storybook/stories/chip/ChipAdvancedStory.tsx
@@ -2,42 +2,67 @@ import React from 'react';
 import ChipStory, {ChipStoryProps} from './ChipStory';
 import {StyleSheet} from 'react-native';
 
-export interface ChipAdvancedStoryProps extends ChipStoryProps {}
+export interface ChipAdvancedStoryProps extends ChipStoryProps {
+  rare?: string[];
+  invalid?: string[];
+}
 
-export default function ChipAdvancedStory({...props}: ChipAdvancedStoryProps) {
-  const rare = ['Ackee', 'Cherimoya', 'Durian'];
-  const invalid = ['Dragon', 'Elephant'];
-
+export default function ChipAdvancedStory({
+  rare = ['Ackee', 'Cherimoya', 'Durian'],
+  invalid = ['Dragon', 'Elephant'],
+  chips = [
+    'Apple',
+    'Ackee',
+    'Melon',
+    'Dragon',
+    'Pear',
+    'Elephant',
+    'Banana',
+    'Cherimoya',
+    'Grape',
+    'Peach',
+    'Durian',
+    'Orange',
+  ],
+  actionType = 'checkbox',
+  chipContainerStyle,
+  selectedChipContainerStyle,
+  rightIconAction,
+  leftIconAction,
+  ...props
+}: ChipAdvancedStoryProps) {
   return (
     <ChipStory
       {...props}
-      actionType="checkbox"
+      chips={chips}
+      actionType={actionType}
       chipContainerStyle={id =>
         StyleSheet.flatten([
           styles.chipContainer,
+          typeof chipContainerStyle === 'function'
+            ? chipContainerStyle(id)
+            : chipContainerStyle,
           rare.includes(id) && styles.rareChipContainer,
           invalid.includes(id) && styles.invalidChipContainer,
         ])
       }
-      selectedChipContainerStyle={styles.selectedContainer}
-      rightIconAction={(id, isSelected) =>
-        invalid.includes(id) && !isSelected ? 'delete' : undefined
+      selectedChipContainerStyle={id =>
+        StyleSheet.flatten([
+          styles.selectedContainer,
+          typeof selectedChipContainerStyle === 'function'
+            ? selectedChipContainerStyle(id)
+            : selectedChipContainerStyle,
+        ])
       }
-      leftIconAction={() => 'check'}
-      chips={[
-        'Apple',
-        'Ackee',
-        'Melon',
-        'Dragon',
-        'Pear',
-        'Elephant',
-        'Banana',
-        'Cherimoya',
-        'Grape',
-        'Peach',
-        'Durian',
-        'Orange',
-      ]}
+      rightIconAction={
+        rightIconAction !== undefined
+          ? rightIconAction
+          : (id, isSelected) =>
+              invalid.includes(id) && !isSelected ? 'delete' : undefined
+      }
+      leftIconAction={
+        leftIconAction !== undefined ? leftIconAction : () => 'check'
+      }
     />
   );
 }
