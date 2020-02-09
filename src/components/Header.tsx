254065ee2b77363b427810523f96bd337ebe0c84
Praya Amadiga Pitasa <prayaamadigapitasa@gmail.com>
2020-02-09 10:54:30 +0700

    Add Header props: [containerStyle, leftContainerStyle, centerContainerStyle, rightContainerStyle]

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
+import {View, StyleSheet, ViewStyle} from 'react-native';
 
-export interface HeaderProps {}
+export interface HeaderProps {
+  containerStyle?: ViewStyle;
+  leftContainerStyle?: ViewStyle;
+  centerContainerStyle?: ViewStyle;
+  rightContainerStyle?: ViewStyle;
+}
 
 export default function Header() {
   return (
-    <>
-      <></>
-    </>
+    <View style={styles.container}>
+      <View style={styles.leftContainer} />
+      <View style={styles.centerContainer} />
+      <View style={styles.rightContainer} />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    paddingTop: 12,
+    backgroundColor: 'dodgerblue',
+    justifyContent: 'space-between',
+    borderBottomWidth: 1,
+    borderBottomColor: 'whitesmoke',
+  },
+  leftContainer: {
+    flex: 1,
+  },
+  centerContainer: {
+    flex: 3,
+  },
+  rightContainer: {
+    flex: 1,
+  },
+});
