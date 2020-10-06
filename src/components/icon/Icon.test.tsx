83c01098064f751530112ce056daf3f48fbe8580
Praya Amadiga Pitasa <prayaamadigapitasa@gmail.com>
2020-10-06 07:47:19 +0700

    test: icon

---
+++ src/components/icon/Icon.test.tsx
@@ -0,0 +1,9 @@
+import {render} from '@testing-library/react-native';
+import React from 'react';
+import Icon from './Icon';
+
+describe('Icon', () => {
+  test('default', async () => {
+    render(<Icon name="" />);
+  });
+});
