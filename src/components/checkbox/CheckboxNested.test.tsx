a18c7cf1aee9b9325e01d38a63a0eb448f0a494c
Praya Amadiga Pitasa <prayaamadigapitasa@gmail.com>
2020-10-15 13:30:58 +0700

    fix: checkbox default status

--- src/components/checkbox/CheckboxNested.test.tsx
+++ src/components/checkbox/CheckboxNested.test.tsx
@@ -1,11 +1,10 @@
-import {render} from '@testing-library/react-native';
 import React from 'react';
+import {render} from '@testing-library/react-native';
 import {ObjectPartial} from '../../types';
 import CheckboxNested, {CheckboxNestedProps} from './CheckboxNested';
 
 const defaultProps: CheckboxNestedProps = {
   checkboxIds: [{title: 'alphabet', checkboxIds: ['a', 'b']}],
-  status: 'not-selected',
 };
 
 function runTest(name: string, props?: ObjectPartial<CheckboxNestedProps>) {
