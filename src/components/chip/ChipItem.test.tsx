8cb1601387cb826e0154d21637085e18c11ba9c9
Praya Amadiga Pitasa <prayaamadigapitasa@gmail.com>
2020-10-15 18:02:19 +0700

    test: chip item

--- src/components/chip/ChipItem.test.tsx
+++ src/components/chip/ChipItem.test.tsx
@@ -1,14 +1,22 @@
-import {render} from '@testing-library/react-native';
+import {fireEvent, render} from '@testing-library/react-native';
 import React from 'react';
 import {ObjectPartial} from '../../types';
 import {ChipItem, ChipItemProps} from './ChipItem';
 
+const defaultProps: ChipItemProps = {
+  testID: 'chip',
+};
+
 function runTest(name: string, props?: ObjectPartial<ChipItemProps>) {
   test(name, async () => {
-    render(<ChipItem {...props} />);
+    const {getByTestId} = render(<ChipItem {...defaultProps} {...props} />);
+    const chip = getByTestId('chip');
+
+    fireEvent(chip, 'layout', {nativeEvent: {layout: {}}});
   });
 }
 
 describe('ChipItem', () => {
   runTest('default');
+  runTest('rounded false', {rounded: false});
 });
