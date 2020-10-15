533545a9e38de824fd297016ce812db20bb1d0e2
Praya Amadiga Pitasa <prayaamadigapitasa@gmail.com>
2020-10-15 14:27:25 +0700

    chore: chip back to nature

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,7 +6,7 @@ export * from './badge';
 export * from './button-group';
 export * from './checkbox';
 export * from './list';
-export * from './chips';
+export * from './chip';
 export * from './Header';
 export * from './Input';
 export * from './InputOTP';
