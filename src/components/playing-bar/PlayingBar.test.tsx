fbfb61b5bd1c2065c74e22554c8c5df2a140119c
Praya Amadiga Pitasa <prayaamadigapitasa@gmail.com>
2020-10-22 12:12:04 +0700

    test: playing bar

---
+++ src/components/playing-bar/PlayingBar.test.tsx
@@ -0,0 +1,14 @@
+import {render} from '@testing-library/react-native';
+import React from 'react';
+import {ObjectPartial} from '../../types';
+import PlayingBar, {PlayingBarProps} from './PlayingBar';
+
+function runTest(name: string, props?: ObjectPartial<PlayingBarProps>) {
+  test(name, async () => {
+    render(<PlayingBar {...props} />);
+  });
+}
+
+describe('PlayingBar', () => {
+  runTest('default');
+});
