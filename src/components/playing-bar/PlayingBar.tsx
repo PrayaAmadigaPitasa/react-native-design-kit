c8678a947c4384e3f08a8b8d8e6fb6b8513b90e0
Praya Amadiga Pitasa <prayaamadigapitasa@gmail.com>
2020-10-22 11:54:44 +0700

    chore: enhance playing bar

--- src/components/playing-bar/PlayingBar.tsx
+++ src/components/playing-bar/PlayingBar.tsx
@@ -1,16 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback, useMemo} from 'react';
 import {View, ViewProps, StyleSheet, ViewStyle, Platform} from 'react-native';
 
-export interface PlayingBarItemBaseProps {
-  barStyle?: ViewProps;
-}
-
-export interface PlayingBarItemProps extends PlayingBarItemBaseProps {
-  height: number;
-}
-
-export interface PlayingBarProps extends PlayingBarItemBaseProps {
+export interface PlayingBarProps {
   bars?: number[];
+  barStyle?: ViewProps;
   containerStyle?: ViewStyle;
   minHeight?: number;
   maxHeight?: number;
@@ -18,29 +11,47 @@ export interface PlayingBarProps extends PlayingBarItemBaseProps {
   frequency?: number;
 }
 
-export function PlayingBarItem({barStyle, height}: PlayingBarItemProps) {
-  return (
-    <View
-      style={StyleSheet.flatten([styles.bar, barStyle, {height: height}])}
-    />
-  );
-}
-
 export default function PlayingBar({
   bars = [0.5, 0.9, 0.4, 0.8],
+  barStyle,
   containerStyle,
   minHeight = 4,
   maxHeight = 20,
   tps = 100,
   frequency = 1.5,
-  ...props
 }: PlayingBarProps) {
   const [progress, setProgress] = useState(0);
-  const min = Math.min(minHeight, maxHeight);
-  const max = Math.max(minHeight, maxHeight);
-  const dif = max - min;
-  const period = 1000 / tps;
-  const increment = (1 / tps) * frequency;
+  const min = useMemo(() => Math.min(minHeight, maxHeight), [
+    minHeight,
+    maxHeight,
+  ]);
+  const max = useMemo(() => Math.max(minHeight, maxHeight), [
+    minHeight,
+    maxHeight,
+  ]);
+  const dif = useMemo(() => max - min, [max, min]);
+  const period = useMemo(() => 1000 / tps, [tps]);
+  const increment = useMemo(() => (1 / tps) * frequency, [tps, frequency]);
+
+  const handleRenderPlayingBarItem = useCallback(
+    (key: number, start: number) => {
+      const relative = (progress + Math.abs(start)) % 1;
+      const height = min + dif * 2 * (relative > 0.5 ? 1 - relative : relative);
+
+      return (
+        <View
+          key={key}
+          style={StyleSheet.flatten([styles.bar, barStyle, {height}])}
+        />
+      );
+    },
+    [barStyle, progress, min, dif],
+  );
+
+  const handleRenderListPlayingBarItem = useMemo(
+    () => bars.map((value, index) => handleRenderPlayingBarItem(index, value)),
+    [handleRenderPlayingBarItem],
+  );
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -53,30 +64,6 @@ export default function PlayingBar({
     };
   }, [progress]);
 
-  function getHeight(start: number) {
-    const relative = (progress + Math.abs(start)) % 1;
-
-    return min + dif * 2 * (relative > 0.5 ? 1 - relative : relative);
-  }
-
-  function ListPlayingBarItem() {
-    const list: JSX.Element[] = [];
-
-    for (let index = 0; index < bars.length; index++) {
-      const bar = (
-        <PlayingBarItem
-          {...props}
-          key={index}
-          height={getHeight(bars[index])}
-        />
-      );
-
-      list.push(bar);
-    }
-
-    return list;
-  }
-
   return (
     <View
       style={StyleSheet.flatten([
@@ -84,7 +71,7 @@ export default function PlayingBar({
         containerStyle,
         {height: maxHeight + 1},
       ])}>
-      {ListPlayingBarItem()}
+      {handleRenderListPlayingBarItem}
     </View>
   );
 }
